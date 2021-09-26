ec2279faf4488969c9ed7a1e4b52da92bf8c64a1
Tcfocus <46071768+Tcfocus@users.noreply.github.com>
2021-09-25 18:48:57 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+API_URL = "https://paper-api.alpaca.markets"
+API_KEY = "yourkey"
+API_SECRET = "yoursecret"
+
+DB_HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
+
+DB_PORT = 5432
